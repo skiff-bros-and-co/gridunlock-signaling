1f069e218d391cef65fa21aab45ff55903933ef6
David Skiff <dskiff@users.noreply.github.com>
2023-07-05 00:49:09 -0400

    allow origins

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"strings"
 
 	"github.com/gorilla/websocket"
 )
+
+const MAX_MESSAGE_BYTES = 1024
 
 type Message struct {
 	Type   string   `json:"type"`
@@ -15,34 +18,42 @@ type Message struct {
 	Data   string   `json:"data,omitempty"`
 }
 
-var upgrader = websocket.Upgrader{}
-var todoList []string
-
-const MAX_MESSAGE_BYTES = 1024
-
-func getCmd(input string) string {
-	inputArr := strings.Split(input, " ")
-	return inputArr[0]
-}
-
-func getMessage(input string) string {
-	inputArr := strings.Split(input, " ")
-	var result string
-	for i := 1; i < len(inputArr); i++ {
-		result += inputArr[i]
-	}
-	return result
-}
-
-func updateTodoList(input string) {
-	tmpList := todoList
-	todoList = []string{}
-	for _, val := range tmpList {
-		if val == input {
-			continue
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		origin := r.Header.Get("Origin")
+		if origin == "" {
+			log.Println("Received empty origin")
+			return false
 		}
-		todoList = append(todoList, val)
-	}
+
+		// local dev builds
+		if origin == " http://localhost:5173/" {
+			return true
+		}
+
+		parsedOrigin, err := url.Parse(origin)
+		if err != nil {
+			log.Println("Received invalid origin:", origin)
+			return false
+		}
+
+		if parsedOrigin.Scheme != "https" {
+			log.Println("Received invalid origin:", origin)
+			return false
+		}
+
+		if parsedOrigin.Hostname() == "gridunlockapp.com" {
+			return true
+		}
+
+		// Dev builds
+		if strings.HasSuffix(parsedOrigin.Hostname(), ".gridunlock-org.pages.dev") {
+			return true
+		}
+
+		log.Println("Received invalid origin:", origin)
+		return false
+	},
 }
 
 func main() {
@@ -57,46 +68,6 @@ func main() {
 	}
 
 	log.Println("Server started at http://localhost:8080")
-
-	http.HandleFunc("/todo", func(w http.ResponseWriter, r *http.Request) {
-		// Upgrade upgrades the HTTP server connection to the WebSocket protocol.
-		conn, err := upgrader.Upgrade(w, r, nil)
-		if err != nil {
-			log.Print("upgrade failed: ", err)
-			return
-		}
-		defer conn.Close()
-
-		conn.SetReadLimit(1024)
-
-		// Continuosly read and write message
-		for {
-			mt, message, err := conn.ReadMessage()
-			if err != nil {
-				log.Println("read failed:", err)
-				break
-			}
-			input := string(message)
-			cmd := getCmd(input)
-			msg := getMessage(input)
-			if cmd == "add" {
-				todoList = append(todoList, msg)
-			} else if cmd == "done" {
-				updateTodoList(msg)
-			}
-			output := "Current Todos: \n"
-			for _, todo := range todoList {
-				output += "\n - " + todo + "\n"
-			}
-			output += "\n----------------------------------------"
-			message = []byte(output)
-			err = conn.WriteMessage(mt, message)
-			if err != nil {
-				log.Println("write failed:", err)
-				break
-			}
-		}
-	})
 
 	http.HandleFunc("/signaling", func(w http.ResponseWriter, r *http.Request) {
 		conn, err := upgrader.Upgrade(w, r, nil)
@@ -128,10 +99,6 @@ func main() {
 				}
 			}
 		}
-	})
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "websockets.html")
 	})
 
 	log.Fatal(http.ListenAndServe(addr, nil))
